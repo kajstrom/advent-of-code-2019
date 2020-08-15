ca6aa85ebf108a6d5ddf0ee582957f9a5d834851
Kaj Ström <kaj.strom@kapsi.fi>
2020-08-15 20:53:02 +0300

    Day 08: Refactoring

--- scala/src/main/scala/day08/Day08.scala
+++ scala/src/main/scala/day08/Day08.scala
@@ -7,39 +7,25 @@ import scala.io.Source
 object Day08 {
   def main(args: Array[String]): Unit = {
     val pixelsPerLayer = 25 * 6
-    val layers: ListBuffer[String] = ListBuffer.empty
+    var layers: List[String] = List.empty
 
-    Source.fromResource("day08.txt").getLines().toList.map(line => {
-      var remainingLine = line
+    val line = Source.fromResource("day08.txt").getLines().toList.head
+    layers = line.grouped(pixelsPerLayer).toList
 
-      while (remainingLine.length > 0) {
-        layers.append(remainingLine.take(pixelsPerLayer))
-        remainingLine = remainingLine.drop(pixelsPerLayer)
-      }
+    val layerWithLeastZeros = layers.minBy(_.count(char => char == '0'))
 
-      val layerWithLeastZeros = layers.minBy(_.count(char => char == '0'))
+    println("Part 1: " + (layerWithLeastZeros.count(_ == '1') * layerWithLeastZeros.count(_ == '2')))
 
-      println("Part 1: " + (layerWithLeastZeros.count(_ == '1') * layerWithLeastZeros.count(_ == '2')))
+    val layersAsLists = layers.map(_.toList)
 
-      val layersAsLists = layers.map(_.toList)
-
-      val image = 0.until(pixelsPerLayer).map(pixelId => {
-        val pixelLayers = layersAsLists.map(_(pixelId))
-
-        pixelLayers.filter(layer => layer != '2')
-          .head
-      })
-
-      var remainingImagePixels = image.toList
-      var text = ""
-
-      while (remainingImagePixels.length > 0) {
-        text += remainingImagePixels.take(25).mkString + "\r\n"
-        remainingImagePixels = remainingImagePixels.drop(25)
-      }
-
-      println("Part 2:")
-      print(text.replace('0', ' '))
+    val image = 0.until(pixelsPerLayer).map(pixelId => {
+      val pixelLayers = layersAsLists.map(_(pixelId))
+      pixelLayers.filter(layer => layer != '2').head
     })
+
+    val text = image.grouped(25).map(_.mkString).mkString("\r\n")
+
+    println("Part 2:")
+    print(text.replace('0', ' '))
   }
 }
